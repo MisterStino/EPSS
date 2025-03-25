0353b27bee157cc21f298f6df60d66957916e69a
MisterStino <stijngroot.business@gmail.com>
2025-03-25 23:23:05 +0100

    Update base_keys.py to change small base keys file path; modify generate_github_features.py to load and display sampled data

--- data/github/scripts/generate_github_features.py
+++ data/github/scripts/generate_github_features.py
@@ -1,5 +1,9 @@
 from data.general_utils.base_keys import load_base_keys
-
-base_keys_df = load_base_keys(return_format="dataframe", small=True)
+import pandas as pd
+small_base_keys_df = load_base_keys(return_format="dataframe", small=True)
+small_sample_df = pd.read_csv('data/general_utils/files/small_sampled_data.csv')
 print("Base keys as DataFrame:")        
-print(base_keys_df.head(100))
+print(small_base_keys_df.head(100), '\n')
+
+print("Sampled data as DataFrame:") 
+print(small_sample_df.head(100))
